af306f046714a80de2db2f46062593b29b6b61fe
Cole Pillars <pillarscole@gmail.com>
2019-03-23 18:06:22 -0400

    adds to dropdown when created

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 //Saves Paste
 function formSubmit() {
+  //Creates Cookie
   var fileName = document.getElementById("myForm").elements[0].value;
   var fileContents = document.getElementById("myForm").elements[1].value;
   var date = new Date();
   date.setTime(date.getTime()+(10*365*24*60*60*1000));
   document.cookie = fileName + "=" + fileContents + "; expires=" + date.toGMTString();
+
+  //Adds to Dropdown
+  var x = document.getElementById("mySelect");
+  var option = document.createElement("option");
+  option.text = fileName;
+  var doesntExist = true;
+  for (i=0; i<x.options.length; i++) {
+    if (x.options[i].value == fileName) { doesntExist = false; }
+  }
+  if (doesntExist) { x.add(option); }
   return false;
 }
 
-//Loads previous past file name and data
+//Loads previous paste file name and data
 function loadPaste() {
-  var option = document.getElementById("myForm2").elements[0].value;
+  var option = document.getElementById("mySelect").value;
   var data = accessCookie(option);
   document.getElementById("myForm").elements[0].value = option;
   document.getElementById("myForm").elements[1].value = data;
@@ -21,11 +32,10 @@ function loadPaste() {
 function accessCookie(cookieName) {
   var name = cookieName + "=";
   var allCookieArray = document.cookie.split(';');
-  for(var i=0; i<allCookieArray.length; i++)
+  for (var i=0; i<allCookieArray.length; i++)
   {
     var temp = allCookieArray[i].trim();
-    if (temp.indexOf(name)==0)
-    return temp.substring(name.length,temp.length);
+    if (temp.indexOf(name)==0) { return temp.substring(name.length,temp.length); }
   }
   return "";
 }
@@ -34,7 +44,7 @@ function accessCookie(cookieName) {
 function addOptions() {
   var x = document.getElementById("mySelect");
   var cookiearray = document.cookie.split(';');
-  for(var i=0; i<cookiearray.length; i++) {
+  for (var i=0; i<cookiearray.length; i++) {
     var option = document.createElement("option");
     option.text = cookiearray[i].split('=')[0];
     x.add(option);
